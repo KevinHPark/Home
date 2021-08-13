3705f930d530092f38eb60e426462ae7085375eb
KevinHPark <kevinhyungsungpark@gmail.com>
2021-08-13 16:40:57 -0400

    Name change

---
+++ cart.js
@@ -0,0 +1,3 @@
+let userName = localStorage.getItem("userName");
+let userH1 = document.getElementById("greet-user");
+userH1.textContent = userName;
--- login.js
+++ login.js
@@ -1,21 +1,22 @@
-let userName = "Guest";
 
+let userName = "Login";
 
-let userH1 = document.querySelector(".Login");
+// userName = window.prompt("type name NOW");
 
-userH1.innerHTML = `Hello, ${userName}`;
+let userH1 = document.querySelector("#loginName");
+userH1.innerHTML = `${userName}`;
 
-
-function addToCart(title, price, picturePath){
-    //Note: this grabs the current cart from localStorage
-
-    let productsInCart = JSON.parse(localStorage.getItem("productsInCart"))
-    //Note: this handles empty cart
-    if (productsInCart === null || productsInCart === undefined){
-        productsInCart = [];
-    }
-    //Task 3: add the item (title,price,picturePath) as an object to productsInCart array
+//Note: we haven't learned about these yet, but its just code that runs when "Add to cart" button is clicked
+// function addToCart(title, price, picturePath){
+//     alert(`${title} added to cart`);
+//     //Note: this grabs the current cart from localStorage
+//     let productsInCart = JSON.parse(localStorage.getItem("productsInCart"))
+//     //Note: this handles empty cart
+//     if (productsInCart === null || productsInCart === undefined){
+//         productsInCart = [];
+//     }
+//     //Task 3: add the item (title,price,picturePath) as an object to productsInCart array
     
-    //Task 3: store updated productsInCart array in localStorage (remember to stringify)
+//     //Task 3: store updated productsInCart array in localStorage (remember to stringify)
     
-}
+// }
