575c2df7f95ff86e5b257ffe3791f56def80fbe8
KevinHPark <kevinhyungsungpark@gmail.com>
2021-08-13 20:50:43 -0400

    loginTest

--- login.js
+++ login.js
@@ -3,20 +3,7 @@ let userName = "Login";
 
 // userName = window.prompt("type name NOW");
 
+let userH1 = localStorage.setItem(userName)
 let userH1 = document.querySelector("#loginName");
 userH1.innerHTML = `${userName}`;
 
-//Note: we haven't learned about these yet, but its just code that runs when "Add to cart" button is clicked
-// function addToCart(title, price, picturePath){
-//     alert(`${title} added to cart`);
-//     //Note: this grabs the current cart from localStorage
-//     let productsInCart = JSON.parse(localStorage.getItem("productsInCart"))
-//     //Note: this handles empty cart
-//     if (productsInCart === null || productsInCart === undefined){
-//         productsInCart = [];
-//     }
-//     //Task 3: add the item (title,price,picturePath) as an object to productsInCart array
-    
-//     //Task 3: store updated productsInCart array in localStorage (remember to stringify)
-    
-// }
