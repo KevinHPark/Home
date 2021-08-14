178b120ecea82a579ee6b6b314f37c6d7f0658ab
KevinHPark <kevinhyungsungpark@gmail.com>
2021-08-14 14:18:38 -0400

    stuff

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
-let users;
+let users = [];
 let serializedUsers = localStorage.getItem("users")
 //let users = JSON.parse(serializedUsers)
-if(serializedUsers === null){
-    users = []
-} else {
-    users = JSON.parse(serializedUsers)
-}
+// if(serializedUsers === null){
+//     users = []
+// } else {
+//     users = JSON.parse(serializedUsers)
+// }
 
 function store() {
     let newUser = document.querySelector("username");
@@ -17,16 +17,9 @@ users.push(valueUser)
 
 localStorage.setItem("username", username.value);
 
-// function checkLogin(){
-//     let userName = document.forms["loginForm"]["username"];
-//     let passWord =document.forms["loginForm"]["password"];
-// }
-// userName = window.prompt("Welcome to our e-commerce site! Please enter your name");
-
-// checkLogin()
-
 serializedUsers = JSON.stringify(users)
 localStorage.setItem("users", serializedUsers)
+
 
 // let userH1 = document.forms["loginForm"]["username"];
 
