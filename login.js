6b151ef2c016e7183aa3bc05576e6f84e5ce1586
KevinHPark <kevinhyungsungpark@gmail.com>
2021-08-13 22:05:37 -0400

    login visuals

--- login.js
+++ login.js
@@ -1,9 +1,6 @@
 
 let userName = "Login";
 
-// userName = window.prompt("type name NOW");
-
-let userH1 = localStorage.setItem(userName)
 let userH1 = document.querySelector("#loginName");
-userH1.innerHTML = `${userName}`;
 
+userH1.innerHTML = `${userName}`;
