39299b3d248fa0c0bbd8e80dea0b6a6b8dd184cf
KevinHPark <kevinhyungsungpark@gmail.com>
2021-08-13 22:38:56 -0400

    Testing localstorage

--- login.js
+++ login.js
@@ -1,6 +1,15 @@
+function checkLogin(){
+    let userName = document.forms["loginForm"]["username"];
+    let passWord =document.forms["loginForm"]["password"];
+}
+// userName = window.prompt("Welcome to our e-commerce site! Please enter your name");
 
-let userName = "Login";
 
-let userH1 = document.querySelector("#loginName");
+
+let userH1 = document.forms["loginForm"]["username"];
+
+userH1 = localStorage.setItem(document.forms["loginForm"]["username"])
+
+alert(localStorage.getItem("userH1"));
 
 userH1.innerHTML = `${userName}`;
