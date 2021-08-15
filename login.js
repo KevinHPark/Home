cc3f9a02a3144953c51a91b1d65bc84fb145e2aa
KevinHPark <kevinhyungsungpark@gmail.com>
2021-08-15 06:26:33 -0400

    Name now works. Product page. Account Page

--- login.js
+++ login.js
@@ -1,18 +1,24 @@
-let users = ["Login"];
+let serializedUsers = localStorage.getItem("users")
 let userH1 = document.querySelector("#loginName");
+
+if(serializedUsers === null){
+    users = []
+} else {
+    users = JSON.parse(serializedUsers)
+    userH1.innerHTML = `${users}`;
+    userH1.setAttribute('href', 'profile.html')
+}
 
 function store() {
     let newUser = document.querySelector("#username");
     let valueUser = newUser.value;
     users.push(valueUser);
-    userH1.innerHTML = `${users}`;
     serializedUsers = JSON.stringify(users);
     localStorage.setItem("users", serializedUsers);
     window.location.href = "index.html";
-  
 }
 
-userH1.innerHTML = `${users}`;
+
 
 // newUser.innerHTML = `${users}`;
 // let userH1 = document.forms["loginForm"]["username"];
@@ -20,10 +26,3 @@ userH1.innerHTML = `${users}`;
 // userH1 = localStorage.setItem(user);
 
 // alert(localStorage.getItem("userH1"));
-
-//let users = JSON.parse(serializedUsers)
-// if(serializedUsers === null){
-//     users = []
-// } else {
-//     users = JSON.parse(serializedUsers)
-// }
--- rotateDisplay.js
+++ rotateDisplay.js
@@ -1,13 +1,13 @@
-var indexValue = 0;
+let indexValue = 0;
 function carousel() {
     setTimeout(carousel, 4000);
-    var x;
     const img = document.querySelectorAll("img");
-    for (x = 0; x < img.length; x++) {
+    for (let x = 0; x < img.length; x++) {
         img[x].style.display = "none";
     }
     indexValue++
     if (indexValue > img.length) { indexValue = 1 }
+    //I genuinely dont know whats wrong with this code. but it doesnt work even if indexValue is undefined even tho it is....
     img[indexValue - 1].style.display = "block";
 }
 carousel();
